f4d889e21d4f1ae8525cc1f8dce821c5190f0d6f
Korede <oyeyemikorede5@gmail.com>
2024-07-24 12:22:12 +0100

    Loader

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
+"use client";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { useState, useEffect } from "react";
+import Preloader from "../components/preloader/preloader";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -9,10 +12,21 @@ const outfit = Outfit({
 });
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <html lang="en">
       <body className={`body`}>
-        <Providers>{children}</Providers>
+        {loading ? (
+          <Preloader loading={loading} />
+        ) : (
+          <Providers>{children}</Providers>
+        )}
       </body>
     </html>
   );
---
+++ components/preloader/preloader.jsx
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import DotLoader from "react-spinners/DotLoader";
+
+const Preloader = ({ loading }) => {
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "#3498db",
+  };
+
+  return (
+    <div className={`preloader ${loading ? "show" : ""}`}>
+      <DotLoader
+        color={"#3498db"}
+        loading={loading}
+        cssOverride={override}
+        size={80}
+      />
+      <style jsx>{`
+        .preloader {
+          position: fixed;
+          transition: 1s ease-in-out;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: var(--background-2);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          opacity: 0;
+          visibility: hidden;
+          z-index: 9999;
+        }
+        .preloader.show {
+          opacity: 1;
+          visibility: visible;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Preloader;
