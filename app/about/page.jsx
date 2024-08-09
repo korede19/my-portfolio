67b251b048bfeae3af8ee174d411ae74bb03d209
Korede <oyeyemikorede5@gmail.com>
2024-08-09 15:02:45 +0100

    update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,19 +1,24 @@
-"use client";
 import React from "react";
 import styles from "./about.module.css";
 import AboutTab from "@/components/aboutTab/aboutTab";
 import Media from "@/components/media/media";
-import { motion } from "framer-motion";
 import Layout from "@/layout";
 
 const AboutPage = () => {
   return (
     <Layout text="ABOUT ME">
       <div className={styles.aboutText}>
-        <p>Hi, I'm Korede Oyeyemi, a passionate web developer and designer with 4 years of experience in HTML, CSS, and JavaScript frameworks like React and Next.js.</p>
         <p>
-          With a keen eye for detail and a love for creating beautiful, user-friendly websites, I bring a unique blend of creativity and technical skill to every project. I'm also
-          proficient with WordPress, enabling me to craft dynamic and responsive websites that meet diverse client needs.
+          Hi, I'm Korede Oyeyemi, a passionate web developer and designer with 4
+          years of experience in HTML, CSS, and JavaScript frameworks like React
+          and Next.js.
+        </p>
+        <p>
+          With a keen eye for detail and a love for creating beautiful,
+          user-friendly websites, I bring a unique blend of creativity and
+          technical skill to every project. I'm also proficient with WordPress,
+          enabling me to craft dynamic and responsive websites that meet diverse
+          client needs.
         </p>
       </div>
       <AboutTab />
