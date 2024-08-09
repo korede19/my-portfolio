bbf8b13a509aa43334030a70f9212e2f3e6becca
Korede <oyeyemikorede5@gmail.com>
2024-08-09 14:50:54 +0100

    update

--- components/toggle/toggle.jsx
+++ components/toggle/toggle.jsx
@@ -1,6 +1,5 @@
 "use client";
 import styles from "./toogle.module.css";
-import { Button } from "@nextui-org/react";
 import { useEffect } from "react";
 import DarkMode from "@/svg/dark";
 import LightMode from "@/svg/light";
@@ -12,17 +11,26 @@ const ThemeToggleButton = () => {
   const theme = useAppSelector((store) => store.theme);
   const dispatch = useDispatch();
   useEffect(() => {
-    if (theme === "dark") document.querySelector("body")?.setAttribute("data-theme", "dark");
+    if (theme === "dark")
+      document.querySelector("body")?.setAttribute("data-theme", "dark");
     else document.querySelector("body")?.setAttribute("data-theme", "light");
   });
 
   return (
     <>
       <div className={styles.btnContain}>
-        <button className={styles.themeBtn} onClick={() => dispatch(setTheme("light"))} disabled={theme === "light"}>
+        <button
+          className={styles.themeBtn}
+          onClick={() => dispatch(setTheme("light"))}
+          disabled={theme === "light"}
+        >
           <DarkMode />
         </button>
-        <button className={styles.themeBtn} onClick={() => dispatch(setTheme("dark"))} disabled={theme === "dark"}>
+        <button
+          className={styles.themeBtn}
+          onClick={() => dispatch(setTheme("dark"))}
+          disabled={theme === "dark"}
+        >
           <LightMode />
         </button>
       </div>
