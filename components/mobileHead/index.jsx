5a429e96397243b915e43c027e69d5a2de68bd9e
Korede <oyeyemikorede5@gmail.com>
2024-08-21 13:32:31 +0100

    Mobile update

--- components/mobileHead/index.jsx
+++ components/mobileHead/index.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 import ThemeToggleButton from "../toggle/toggle";
+import Link from "next/link";
+import MenuOpen from "@/svg/menuOpen";
+import MenuClose from "@/svg/menuClose";
+import Arrow from "@/svg/arrow";
 
 const MobileHead = ({ Pagename }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <div className={styles.MobileHead}>
@@ -14,7 +24,38 @@ const MobileHead = ({ Pagename }) => {
             <ThemeToggleButton />
           </div>
           <div>
-            <p>menu</p>
+            <nav className={styles.navbar}>
+              <div className={styles.hamburger} onClick={toggleMenu}>
+                {isOpen ? <MenuClose /> : <MenuOpen />}
+              </div>
+              <div
+                className={`${styles.overlay} ${
+                  isOpen ? styles.showOverlay : ""
+                }`}
+              >
+                <ul className={styles.menu}>
+                  <li>
+                    <Link href="/">Home</Link>
+                  </li>
+                  <Arrow />
+                  <li>
+                    <Link href="/about">About Me</Link>
+                  </li>
+                  <Arrow />
+                  <li>
+                    <Link href="/portfolio">Portfolio</Link>
+                  </li>
+                  <Arrow />
+                  <li>
+                    <Link href="/testimonials">Testimonials</Link>
+                  </li>
+                  <Arrow />
+                  <li>
+                    <Link href="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
           </div>
         </div>
       </div>
---
+++ svg/menuOpen.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MenuOpen = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="var(--color)"
+          d="M3 4h18v2H3zm6 7h12v2H9zm-6 7h18v2H3z"
+        ></path>
+      </svg>
+    </>
+  );
+};
+
+export default MenuOpen;
